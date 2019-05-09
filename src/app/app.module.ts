963d4087fea1a07ab87176ed32ff1dc625d8726f
pavan anand chinthalapudi <34681059+pavananand@users.noreply.github.com>
2019-05-09 19:39:42 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+const appRoutes: Routes = [
+  { path: '',
+    redirectTo: '/insertData',
+    pathMatch: 'full'
+  },
+  { path: 'insertData', component: CreatepageComponent },
+  { path: 'retrieveData',  component: RetrivepageComponent }
+];
 
 import { AppComponent } from './app.component';
 
